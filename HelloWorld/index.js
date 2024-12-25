466d12dd4acdd58212d32fa758be1d4e3c5deeb3
Vedansh Bhardwaj <vedwaj@gmail.com>
2024-12-25 13:47:14 +0530

    ep 3 - first instance of using Component Composition- chaining basic components using Functional Components in different ways

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -6,5 +6,23 @@ const heading = [React.createElement("h1", {}, "Here goes the react heading bhag
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("baseElement"));
 const jsxHeading = <h1>Ye to JSX ki heading hai</h1>;
-root.render(heading);
-setTimeout(() => root.render(jsxHeading), 5000);
+// root.render(heading);
+// setTimeout(() => root.render(jsxHeading), 5000);
+
+const UniversalComponent = () => <h1 id="universalHeading">All Inside the UNIVERSE</h1>
+const FirstFunctionalComponent = function(){
+    return (
+            <div id="parentDiv">
+                <h3>This is parent Div heading.</h3>
+                <UniversalComponent/>
+            </div>);
+};
+
+const SecondFunctionalComponent = () => {
+    return <div id="childDiv"> <h5>This is the child Div heading.</h5> <FirstFunctionalComponent/></div>
+}
+
+root.render(<FirstFunctionalComponent/>);
+root.render(<SecondFunctionalComponent/>);
+
+
