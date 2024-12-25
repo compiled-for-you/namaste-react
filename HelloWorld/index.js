6188d3729c8ea797aae8173bc2b0c8e394e022a0
Vedansh Bhardwaj <vedwaj@gmail.com>
2024-12-25 13:11:07 +0530

    episode 3 laying foundation, using JSX for the first time

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,5 +1,10 @@
-const heading = [React.createElement("h1", {}, "Here goes the react heading..."), 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = [React.createElement("h1", {}, "Here goes the react heading bhagg..."), 
     React.createElement("h3", {}, "Here goes the smaller react heading...")];
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("baseElement"));
+const jsxHeading = <h1>Ye to JSX ki heading hai</h1>;
 root.render(heading);
+setTimeout(() => root.render(jsxHeading), 5000);
